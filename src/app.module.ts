1f02fff0625b026f9db0a7710d3ff720c78b3a3d
victoresende19 <victoresende19@gmail.com>
2022-12-11 12:14:15 -0300

    :star: moved user to new api folder :star:

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
+import { UsersModule } from './api/users/users.module';
 
 @Module({
   imports: [MongooseModule.forRoot('mongodb://localhost/users'), UsersModule],

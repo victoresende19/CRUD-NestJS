304ddf83dc5f467f17350c5739281aafd04bec1b
victoresende19 <victoresende19@gmail.com>
2022-12-11 12:01:35 -0300

    :start: LoggerDeleteMiddleware implementation :star:

---
+++ src/users/logger.middleware.ts
@@ -0,0 +1,12 @@
+import { Injectable, NestMiddleware } from "@nestjs/common";
+import { NextFunction, Request, Response } from "express";
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware{
+    use(req: Request, res: Response, next: NextFunction){
+        console.log(`Logging DELETE request Ip: ${req.ip}`);
+        console.log(`Logging DELETE request Path: ${req.path}`);
+        console.log(`Logging DELETE request Headers: ${req.headers}`);
+        next();
+    }
+}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,5 +1,6 @@
-import { Module } from "@nestjs/common";
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
+import { LoggerMiddleware } from "./logger.middleware";
 import { User, UserSchema } from "./schemas/user.schema";
 import { UsersController } from "./users.controller";
 import { UsersRepository } from "./users.repository";
@@ -10,4 +11,11 @@ import { UsersService } from "./users.service";
   controllers: [UsersController],
   providers: [UsersService, UsersRepository]
 })
-export class UsersModule { }
+
+export class UsersModule implements NestModule {
+  configure(consumer: MiddlewareConsumer){
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes({ path: 'users/*', method: RequestMethod.DELETE });
+  }
+}
